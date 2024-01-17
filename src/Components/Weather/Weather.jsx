d8f28f80008b956d9189b2de7594c2109288cfd0
Hyun <hwn123h@naver.com>
2024-01-17 16:05:43 +0900

    날씨 앱 완성

--- src/Components/Weather/Weather.jsx
+++ src/Components/Weather/Weather.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Weather.css'
 import search_icon from './Assets/search.png'
 import clear_icon from './Assets/clear.png'
@@ -6,29 +6,66 @@ import cloud_icon from './Assets/cloud.png'
 import drizzle_icon from './Assets/drizzle.png'
 import humidity_icon from './Assets/humidity.png'
 import wind_icon from './Assets/wind.png'
+import rain_icon from './Assets/rain.png'
 import snow_icon from './Assets/snow.png'
 export const Weather = () => {
 
 
     let api_key = "2d9656e12a5cfa0fd6b7cbebd84d6e23";
 
+    const [wicon, setWicon] = useState(cloud_icon);
+
     const search = async () => {
-        const city = document.getElementsByClassName("cityInput");
-        if (city[0].value === "") {
+        const cityInput = document.getElementById("cityInput");
+        if (!cityInput || !cityInput.value) {
             return;
         }
-        let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${api_key}&lang=kr`
-        let response = await fetch(url);
-        let data = await response.json();
-        const humidity = document.getElementsByClassName("humidity-percent");
-        const wind = document.getElementsByClassName("wind-rate");
-        const temp = document.getElementsByClassName("weather-temp");
-        const location = document.getElementsByClassName("weather-location");
+        try {
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&units=metric&appid=${api_key}&lang=kr`
+            let response = await fetch(url);
 
-        humidity[0].innerHTML = data.main.humidity;
-        wind[0].innerHTML = data.main.wind.speed;
-        temp[0].innerHTML = data.main.temp;
-        location[0].innerHTML = data.name;
+            let data = await response.json();
+
+
+            const humidity = document.getElementsByClassName("humidity-percent")[0];
+            const wind = document.getElementsByClassName("wind-rate")[0];
+            const temp = document.getElementsByClassName("weather-temp")[0];
+            const location = document.getElementsByClassName("weather-location")[0];
+
+            humidity.innerHTML = data.main.humidity + "  %";
+            wind.innerHTML = Math.floor(data.wind.speed) + " Km/h";
+            temp.innerHTML = Math.floor(data.main.temp) + " °C";
+            location.innerHTML = data.name;
+
+            if (data.weather[0].icon === "01d" || data.weather[0].icon === "01n") {
+                setWicon(clear_icon);
+            }
+            else if (data.weather[0].icon === "02d" || data.weather[0].icon === "02n") {
+                setWicon(cloud_icon);
+            }
+            else if (data.weather[0].icon === "03d" || data.weather[0].icon === "03n") {
+                setWicon(drizzle_icon);
+            }
+            else if (data.weather[0].icon === "04d" || data.weather[0].icon === "04n") {
+                setWicon(drizzle_icon);
+            }
+            else if (data.weather[0].icon === "09d" || data.weather[0].icon === "09n") {
+                setWicon(rain_icon);
+            }
+            else if (data.weather[0].icon === "10d" || data.weather[0].icon === "10n") {
+                setWicon(rain_icon);
+            }
+            else if (data.weather[0].icon === "13d" || data.weather[0].icon === "13n") {
+                setWicon(snow_icon);
+            } else {
+                setWicon(clear_icon);
+            }
+
+
+
+        } catch (err) {
+            console.error('Error during data fetch', err);
+        }
 
     }
 
@@ -36,13 +73,13 @@ export const Weather = () => {
     return (
         <div className='container'>
             <div className='top-bar'>
-                <input type="text" className="cityInput" placeholder='Search' />
+                <input type="text" className="cityInput" id="cityInput" placeholder='Search' />
                 <div className="search-icon" onClick={() => { search() }}>
                     <img src={search_icon} alt="" />
                 </div>
             </div>
             <div className='weather-img'>
-                <img src={cloud_icon} alt="" />
+                <img src={wicon} alt="" />
             </div>
             <div className="weather-temp">24°C</div>
             <div className="weather-location">Seoul</div>
@@ -50,7 +87,7 @@ export const Weather = () => {
                 <div className="element">
                     <img src={humidity_icon} alt="" className="icon" />
                     <div className="data">
-                        <div className="humidity-percent">64%</div>
+                        <div className="humidity-percent">64 %</div>
                         <div className="text">Humidity</div>
                     </div>
                 </div>
